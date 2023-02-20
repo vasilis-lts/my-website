e17a23e904c5c5748cad1dbae32bb974382c532d
vasilis_litsas <vasilis.litsas@architechts.gr>
2023-02-21 00:15:24 +0200

    updated about me page text

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -33,12 +33,12 @@ export default function Projects() {
 
         <div className={styles.center} style={{ width: '90%', maxWidth: 500, textAlign: 'justify', alignItems: "initial", lineHeight: '1.4rem' }}>
           <p>Hello dear visitor!</p>
-          <p style={{ marginTop: 20 }}>I am a front end developer based in <b>Athens, Greece,</b> with 7 years of experience. My first 2 years as a developer, I was working on websites using <b>Wordpress</b>, before I moved to <b>React</b> apps. My previous job was working remotely at Architechts, a company based in the Netherlands.</p>
+          <p style={{ marginTop: 20 }}>I am a front end developer based in <b>Athens, Greece,</b> with 7 years of experience. I started working on websites, for my first 2 years, using <b>Wordpress</b>, then I moved to <b>React</b> apps. My previous job was working remotely at Architechts, a company based in the Netherlands.</p>
           <p style={{ marginTop: 20 }}>I am currently available for work.</p>
 
         </div>
         <div style={{ marginTop: 40, width: '90%', maxWidth: 500 }} className="flex-center-y">
-          <p>For some additional info check out my LinkedIn profile! </p>
+          <p>For additional info check out my Linked In profile! </p>
           <a href="https://www.linkedin.com/in/vasilis-litsas/" target="_blank" rel="noreferrer"><LinkedInIcon style={{ fontSize: 42, color: "#064787" }} /></a>
         </div>
 
